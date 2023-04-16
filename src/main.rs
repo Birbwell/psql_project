c163689aae901f93402add9ed38156517b421bb1
John Birdwell <birdwell@mines.edu>
2023-04-16 15:48:35 -0600

    add custom query option

--- src/main.rs
+++ src/main.rs
@@ -4,7 +4,6 @@ use postgres::{Client, NoTls};
 type Result<T> = std::result::Result<T, Box<dyn std::error::Error>>;
 
 fn main() -> Result<()> {
-
     let mut client = loop {
         let Ok(user) = Text::new("Enter the username:").prompt() else {
             return Ok(())
@@ -30,14 +29,16 @@ fn main() -> Result<()> {
 
     loop {
         let Ok(choice) = Select::new("Main Menu", vec![
-            "Search",
+            "Search For Correlation",
+            "Custom Query",
             "Quit"
         ]).prompt() else {
             break
         };
 
         match choice {
-            "Search" => search_db(&mut client),
+            "Search For Correlation" => search_db_for_correlation(&mut client),
+            "Custom Query" => custom_query(&mut client),
             "Quit" => break,
             _ => {
                 println!("Invalid Input");
@@ -48,11 +49,51 @@ fn main() -> Result<()> {
     Ok(())
 }
 
-fn search_db(client: &mut Client) {
-    let choice = Text::new("Enter the chain you wish to search for:").prompt().unwrap();
-    for row in client.query("select distinct county, state from chain where chain = $1", &[&choice]).unwrap() {
-        let county: String = row.get(0);
-        let state: String = row.get(1);
-        println!("{}, {}", county, state);
+fn search_db_for_correlation(client: &mut Client) {
+    let Ok(choice) = Text::new("Enter the chain you're looking for:").prompt() else {
+        return
+    };
+
+    let query = "with chains as (
+      SELECT * from chain_count
+      WHERE chain_count.chain IN (SELECT cc.chain FROM chain_count AS cc group BY chain HAVING COUNT(*)>19)
+      AND chain_count.chain IN (SELECT cc.chain FROM (SELECT DISTINCT chain_count.chain AS chain, chain_count.count AS count FROM chain_count) AS cc GROUP BY chain HAVING COUNT(*)>1)
+    )
+    SELECT c, chain
+    FROM (SELECT CORR(CAST(crime_1.crime_total AS DOUBLE PRECISION) / crime_1.population, chains.count) as c, chains.chain
+    FROM crime_1, chains
+    WHERE chains.county_id = crime_1.county_id
+    GROUP BY chains.chain
+    ORDER BY c desc) as corrs";
+
+    let Ok(r) = client.query(query, &[]) else {
+        println!("There was an issue running your query");
+        return
+    };
+
+    for row in r {
+        let c: String = row.get(1);
+        if choice == c {
+            let corr: f64 = row.get(0);
+            println!("{} | {}", choice, corr);
+        }
+    }
+}
+
+fn custom_query(client: &mut Client) {
+    let Ok(query) = Text::new("Enter your query:").prompt() else {
+        return
+    };
+    let Ok(r) = client.query(&query, &[]) else {
+        println!("There was something wrong with your query");
+        return
+    };
+    for row in r {
+        let mut line = String::new();
+        for n in 0..row.len() {
+            let v: String = row.get(n);
+            line = format!("{} {}", line, v);
+        }
+        println!("{}", line);
     }
 }
